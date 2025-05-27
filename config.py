bcc2790c5cc8a8e50080095f94722596441c565a
ThinhQuocLe007 <thinhquocle007@gmail.com>
2025-05-27 11:07:52 +0700

    Init

---
+++ config.py
@@ -0,0 +1,10 @@
+# Mysql config 
+MYSQL_HOST = 'localhost' 
+MYSQL_DATABASE = 'tracker' 
+MYSQL_USER = 'quocthinh' 
+MYSQL_PASSWORD = 'quocthinh'
+MYSQL_TABLE = 'notion_data'
+
+# Notion API
+DATABASE_ID = '7decdbb42cbf4aa1bb16e7083b427c09'
+NOTION_API_KEY = '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'
